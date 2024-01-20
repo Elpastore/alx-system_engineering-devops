ab7d805bfa4e190b6084dbf2533c50f602d50339
elpastore <abdoulayesadio1997@gmail.com>
2024-01-20 21:37:45 +0000

    advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ssh configuraiton using puppet
+
+file { 'ect/ssh/ssh_config':
+ ensure  => present,
+ content => "
+	
+	#SSH client configuration
+	host*
+	IdentifFile ~/.ssh/school
+	PasswordAuthentication no",
+}
