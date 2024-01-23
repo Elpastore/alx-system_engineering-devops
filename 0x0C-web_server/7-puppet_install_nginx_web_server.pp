35e0d6beb09bf9e4b35e5390b71b051c4c5c70c3
elpastore <abdoulayesadio1997@gmail.com>
2024-01-23 16:25:04 +0000

    advanced

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Conffiguration of web server using puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World',
+}
+
+file_line { 'Add redirection, 301':
+  path   => '/etc/nginx/sites-available/default',
+  ensure => 'present',
+  after  => 'listen 80 default_server',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
