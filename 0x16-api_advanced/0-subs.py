2423fed0ab34a9f401a078439b634d5512c4cfd4
elpastore <abdoulayesadio1997@gmail.com>
2024-03-05 20:32:46 +0000

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+query module
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Abdoulaye Sadio'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 404:
+        return 0
+    else:
+        subredit_data = response.json().get('data')
+        return subredit_data.get('subscribers')
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+This project is about advanced api, with an overview of pagination, and API documentation
